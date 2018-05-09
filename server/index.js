520da39441c62f539aa746d3fb303d5f7b6fe994
ccficorilli <ccficorilli@gmail.com>
2018-05-09 13:33:15 -0700

    retooling for heroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 /* eslint no-console: "off" */
-import { listen } from "./server";
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static('dist'));
+app.use(express.static('public'));
+
 
 const PORT = process.env.PORT || 3000;
 
-listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
